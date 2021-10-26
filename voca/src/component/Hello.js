b0f32854a5643f024b3076fa454e3537f32a42e2
mygirok <forgirok@gmail.com>
2021-10-26 20:46:49 +0900

    module css

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -9,13 +9,21 @@ const Hello = () => {
 export default Hello;*/
 
 import World from "./World";
+import styles from "./Hello.module.css";
 
 export default function Hello(){
     return (
         <div>
-            <h2>Hello</h2>
-            <World />
-            <World />
+            <h1 style={{
+                    color : "#f00",
+                    borderRight : "12px solid #000",
+                    marginBottom : "50px",
+                    opactiy : 1,
+                }}
+            >
+                Hello
+            </h1>
+            <div className={styles.box}>Hello</div>
         </div>
     )
 } 
