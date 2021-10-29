94ac73c8dff44ca894e82b2386a6510dc030bc5f
mygirok <forgirok@gmail.com>
2021-10-29 20:36:45 +0900

    props

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Hello from './component/Hello';
-import Welcome from './component/Welcome';
-import styles from './App.module.css';
 
 function App() {
-  return <div className="App">
-    <Hello />
-    <Hello />
-    <Hello />
-  </div>
+  return (
+    <div className="App">
+      <h3>props : propertise</h3>
+      <Hello age={10}/>
+      <Hello age={20}/>
+      <Hello age={30}/>
+    </div>
+  );
 }
 
 export default App;
--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello() {
- //   let name = "Mike";
+export default function Hello({ age }) {
     const [name, setName] = useState('Mike');
+    const msg = age > 19 ? "you are adult." : "you are studnet.";
     return (
         <div> 
-            <h2 id="name">{name}</h2>
+            <h2 id="name">
+                {name}({age}) : {msg}
+            </h2>
+            <UserName name={name} />
             <button 
                 onClick={() => {
                     setName(name === "Mike" ? "Jane" : "Mike");
